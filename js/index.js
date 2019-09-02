3992279b964822195a40d3dbdbdc24abbf336e89
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-09-02 16:59:29 +0300

    started on header and main section DOM

--- js/index.js
+++ js/index.js
@@ -39,7 +39,34 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-let figures = document.getElementsByClassName("nav-item-1");
-figures.setAttribute(siteContent["nav"]["nav-item-1"]);
+let nav = document.querySelectorAll("a");
+nav[0].textContent = siteContent["nav"]["nav-item-1"];
+nav[1].textContent = siteContent["nav"]["nav-item-2"];
+nav[2].textContent = siteContent["nav"]["nav-item-3"];
+nav[3].textContent = siteContent["nav"]["nav-item-4"];
+nav[4].textContent = siteContent["nav"]["nav-item-5"];
+nav[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let ctaText = document.querySelectorAll(".cta .cta-text h1");
+ctaText[0].textContent = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelectorAll(".cta .cta-text button");
+ctaButton[0].textContent = siteContent["cta"]["button"];
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let mainContent = document.getElementsByClassName("text-content");
+mainContent[0].textContent = siteContent["main-content"]["features-h4"];
+mainContent[1].textContent = siteContent["main-content"]["about-h4"];
+mainContent[2].textContent = siteContent["main-content"]["services-h4"];
+mainContent[3].textContent = siteContent["main-content"]["product-h4"];
+mainContent[4].textContent = siteContent["main-content"]["vision-h4"];
+
+
+
+
+
+
